cb8e687a777501316fa5ff1b9612952e64a89ce8
Nicolas Espino <nicolasespino@Nicolass-MBP.hsd1.ca.comcast.net>
2020-03-28 14:51:38 -0700

    Completed set_at and get functions

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -7,15 +7,15 @@ using std::string;
 
 TopTenList::TopTenList()
 {
-
+  _list.resize(10);
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
 {
-
+  _list[index-1] = link;
 }
 
 Hyperlink TopTenList::get(int index)
 {
-
+  return _list[index-1];
 }
