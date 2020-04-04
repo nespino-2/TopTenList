52cae12241be876abe152ae1fac989bf8859db39
Nicolas Espino <nicolasespino@Nicolass-MBP.hsd1.ca.comcast.net>
2020-04-03 21:45:50 -0700

    Updated on 4/3/2020

--- TopTenList.cpp
+++ TopTenList.cpp
@@ -1,13 +1,27 @@
 //Manages a list of top ten hyperlinks
+#include <iostream>
 #include <string>
+#include <set>
 #include "TopTenList.h"
 #include "Hyperlink.h"
 
-using std::string;
+
+using namespace std;
 
 TopTenList::TopTenList()
 {
   _list.resize(10);
+}
+
+void TopTenList::display_forward(){
+    // for (auto =it;it<_list.begin();++it) {
+    //   cout >> _list[it];
+    // }
+
+}
+
+void ToopTenList::display_backward() {
+
 }
 
 void TopTenList::set_at(int index, Hyperlink link)
--- TopTenList.h
+++ TopTenList.h
@@ -3,17 +3,21 @@
 #define TOPTENLIST_H
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "Hyperlink.h"
+#include "IDirectionable.h"
 
 using std::string;
 using std::vector;
 
-class TopTenList
+class TopTenList : public IDirectionable
 {
   private:
     vector<Hyperlink> _list;
   public:
     TopTenList();
+    void display_forward();
+    void display_backward();
     void set_at(int index, Hyperlink link);
     Hyperlink get(int index);
 };
